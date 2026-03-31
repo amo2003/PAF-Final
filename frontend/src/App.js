f4ac59e6ed318b217999867f35af8242d70b9aa3
Amod Indupa <amodindupa9@gmail.com>
2026-03-31 21:11:34 +0530

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import BookingList from './M2/pages/BookingList';
+import BookingForm from './M2/pages/BookingForm';
+import BookingDetail from './M2/pages/BookingDetail';
+import MyBookings from './M2/pages/MyBookings';
+import AdminPanel from './M2/pages/AdminPanel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bookings" element={<BookingList />} />
+          <Route path="/bookings/:id" element={<BookingDetail />} />
+          <Route path="/book" element={<BookingForm />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/admin" element={<AdminPanel />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
